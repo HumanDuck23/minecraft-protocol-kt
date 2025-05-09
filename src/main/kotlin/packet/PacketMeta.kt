2e756ffa6b45412af110c52d5b127fa38b02b012
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2025-05-09 13:44:01 +0200

    remove type from meta

--- src/main/kotlin/packet/PacketMeta.kt
+++ src/main/kotlin/packet/PacketMeta.kt
@@ -5,6 +5,5 @@ import kotlin.reflect.KClass
 data class PacketMeta(
     val id: Int,
     val direction: PacketDirection,
-    val state: ProtocolState,
-    val type: KClass<out Packet>
+    val state: ProtocolState
 )
