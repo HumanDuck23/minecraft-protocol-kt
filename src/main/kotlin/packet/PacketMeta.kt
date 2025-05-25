249d2094ea4ba2b1bbf3766ee619553bdff1d047
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2025-05-26 00:31:16 +0200

    better PacketMeta toString()

--- src/main/kotlin/packet/PacketMeta.kt
+++ src/main/kotlin/packet/PacketMeta.kt
@@ -4,4 +4,8 @@ data class PacketMeta(
     val id: Int,
     val direction: PacketDirection,
     val state: ProtocolState
-)
+) {
+    override fun toString(): String {
+        return "PacketMeta(id=0x${id.toString(16)}, direction=$direction, state=$state)"
+    }
+}
