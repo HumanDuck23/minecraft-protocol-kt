90af562a6c134d69acaa59415b60283db279f722
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2025-05-27 16:45:13 +0200

    fix ByteArrayValue to have a length to read

--- src/main/kotlin/packet/values/ByteArrayValue.kt
+++ src/main/kotlin/packet/values/ByteArrayValue.kt
@@ -5,13 +5,23 @@ import dev.spaghett.packet.PacketValue
 import io.netty.buffer.ByteBuf
 
 class ByteArrayValue(
-    parent: Packet,
+    private val parent: Packet,
+    private val lengthProvider: (() -> Int)? = null,
     value: ByteArray = byteArrayOf(),
     readPredicate: (() -> Boolean)? = null
 ) : PacketValue<ByteArray>(parent, value, readPredicate) {
 
     override fun read(buffer: ByteBuf) {
-        val length = buffer.readableBytes()
+        val length = lengthProvider?.invoke()
+            ?: buffer.readableBytes()       // no provider means everything left
+
+        check(length >= 0) {
+            "Invalid length ($length) for ${parent.meta} on ByteArrayValue"
+        }
+        check(buffer.readableBytes() >= length) {
+            "Not enough bytes for ${parent.meta}: have=${buffer.readableBytes()}, need=$length"
+        }
+
         val bytes = ByteArray(length)
         buffer.readBytes(bytes)
         value = bytes
