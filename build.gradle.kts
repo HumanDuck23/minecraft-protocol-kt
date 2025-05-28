c358b8b64e83012aaa7f7206f2743be7df08aa97
HumanDuck23 <44237686+HumanDuck23@users.noreply.github.com>
2025-05-28 15:10:57 +0200

    version, remove unused dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,7 +3,7 @@ plugins {
 }
 
 group = "dev.spaghett"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
@@ -17,7 +17,6 @@ dependencies {
     implementation("org.slf4j:slf4j-simple:2.0.13")
     implementation("com.google.code.gson:gson:2.10.1")
     implementation("com.squareup.okhttp3:okhttp:4.12.0")
-    implementation("com.microsoft.azure:msal4j:1.20.1")
 
     // https://mvnrepository.com/artifact/net.raphimc/MinecraftAuth
     implementation("net.raphimc:MinecraftAuth:4.1.1")
